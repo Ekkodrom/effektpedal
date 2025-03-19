4636cbf9a7f93fbcfd54b9ebaf454460b15cd7b4
Ekkodrom <superlauth@hotmail.com>
2025-03-19 13:51:01 +0100

    open jack and SC in main script

--- EffectPedal/main.py
+++ EffectPedal/main.py
@@ -1,30 +1,39 @@
+import subprocess
+import time
 from PyQt5.QtWidgets import QApplication
 from gui import EffectPedalGUI
 from audio_engine.audio_input import AudioInput
-# from motion_control.motion_tracker import MotionTracker  # Commented out for now
 from pythonosc import udp_client
-
-osc_client = udp_client.SimpleUDPClient("127.0.0.1", 57120)  # SC listens here
-osc_client.send_message("/setFreq", 440)  # send a frequency value
-
+import os
 
 class MainApp:
     def __init__(self):
+        self.start_jack()
+        self.start_supercollider()
+        time.sleep(2)  # Wait a bit for SuperCollider to initialize
+
         self.app = QApplication([])
         self.gui = EffectPedalGUI()
         self.audio = AudioInput()
-        # self.motion_tracker = MotionTracker()  # Commented out for now
 
-        self.init_systems()
+    def start_jack(self):
+        """Start the JACK audio server with the correct settings."""
+        print("Starting JACK server...")
+        jack_command = [
+            "jackd", "-d", "alsa", "-d", "plughw:1,0",
+            "-r", "44100", "-p", "1024", "-n", "2"
+        ]
+        subprocess.Popen(jack_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(3)  # Give JACK some time to start
 
-    def init_systems(self):
-        # Initialize audio input
-        self.audio.start()
-        
-        # Initialize motion tracking (commented out)
-        # self.motion_tracker.start_tracking()
+    def start_supercollider(self):
+        """Start SuperCollider server."""
+        print("Starting SuperCollider...")
+        scsynth_command = ["scsynth", "-u", "57110"]
+        subprocess.Popen(scsynth_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     def run(self):
+        """Run the GUI."""
         self.gui.show()
         self.app.exec_()
 
